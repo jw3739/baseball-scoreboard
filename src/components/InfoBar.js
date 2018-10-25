70110617048db62181f5847b7014db33f6edbb58
Jim White <jw3739@nyu.edu>
2018-10-25 16:43:57 -0400

    arrows and more layout optimization

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -4,7 +4,13 @@ class InfoBar extends Component {
   render() {
     return (
       <div className="info-bar-container">
-        <span className='inning-container'>^1</span>
+        <span className='inning-container'>
+            <div className='arrow-container'>
+                <div class="arrow-up"></div>
+                <div class="arrow-down"></div>
+            </div>
+            1
+        </span>
         <span className='outs-container'>0 Out</span>
         <span className='count-container'>0-1</span>
       </div>
