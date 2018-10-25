4b7515cfc120b3b37deef678c14b64c2ef2d345d
Jim White <jw3739@nyu.edu>
2018-10-25 16:31:59 -0400

    font sizes, better colors, layout tweaks (width). basic static layout complete

--- src/components/TeamScore.js
+++ src/components/TeamScore.js
@@ -4,8 +4,14 @@ class TeamScore extends Component {
   render() {
     return (
       <div className="team-score-container">
-        <div className='team-1'>BOS 1</div>
-        <div className='team-2'>LA 0</div>
+        <div className='team team-1'>
+            <span className='team-name'>BOS</span> 
+            <span className='score'>1</span>
+        </div>
+        <div className='team team-2'>
+            <span className='team-name'>LA</span> 
+            <span className='score'>0</span>
+        </div>
       </div>
     );
   }
