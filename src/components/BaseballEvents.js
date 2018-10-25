89df27da9b3335ff29e5f8688580cd6984e8412c
Jim White <jw3739@nyu.edu>
2018-10-25 16:52:51 -0400

    add events and organize into catagories

--- src/components/BaseballEvents.js
+++ src/components/BaseballEvents.js
@@ -5,10 +5,29 @@ class BaseballEvents extends Component {
   render() {
     return (
       <div className="misc-container">
-        <div className='buttons-container'>
-            <button>Hit</button>
-            <button>Out</button>
-        </div>
+          <div className='buttons-container'>
+            <div className='hits-container'>
+              <button>Single</button>
+              <button>Double</button>
+              <button>Triple</button>
+              <button>Homerun</button>
+            </div>
+            <div className='pitch-container'>
+              <button>Strike</button>
+              <button>Ball</button>
+              <button>Foul</button>
+            </div>
+            <div className='out-event-container'>
+              <button>Out</button>
+              <button>Doubleplay</button>
+              <button>Tripleplay</button>
+            </div>
+            <div className='misc-event-container'>
+              <button>Stolen Base</button>
+              <button>Sac Fly</button>
+              <button>Pickoff</button>
+            </div> 
+          </div>
         <div className='scoreboard-container'>
             <Scoreboard />
         </div>
